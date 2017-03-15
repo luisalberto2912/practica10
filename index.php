fb0831f20afed54a64fb7d7a781dd593a3966f10
luisalberto2912 <abrandi261@gmail.com>
2017-03-15 15:36:19 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//comentarios
+#comentarios
+/*comentarios
+multilinea*/
+echo "Hola mundo php";
+?>
+<html>
+<head>
+<title>Practica10</title>
+</head>
+<body>
+<h1>Hola mundo html</h1>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+<title>Practica 10 php</title>
+</head>
+<body>
+<h1>Usando php</h1>
+<p><?php
+echo "parrafo php";
+?></p>
+<p>Parrafo HTML</p>
+<h3>
+<?php
+echo "parrafo php";
+?>
+</h3>
+</body>
+</html>
